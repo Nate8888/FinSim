dd704ce074375b2c8a60e63af165e25a7c08024c
Nate8888 <wilk.nathan@gmail.com>
2024-12-01 17:58:49 -0500

    update backend for gcloud app deployment

--- backend/main.py
+++ backend/main.py
@@ -863,5 +863,10 @@ def check_user_round_completion():
 
     return jsonify({'userCompleted': user_completed}), 200
 
+@app.route('/readines_check', methods=['GET'])
+@cross_origin()
+def readiness_check():
+    return jsonify({'status': 'ready'}), 200
+
 if __name__ == '__main__':
     app.run(debug=True)
