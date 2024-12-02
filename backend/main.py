56400167401c77f02b151024fb9493c0951d70bb
Nate8888 <wilk.nathan@gmail.com>
2024-12-01 19:53:54 -0500

    fixes for prod

--- backend/main.py
+++ backend/main.py
@@ -334,9 +334,6 @@ def generate_all_rounds(rounds, time_per_round):
         new_round['round_id'] = generate_round_id()
         market_data.append(new_round)
         previous_round = copy.deepcopy(new_round)  # Make a deep copy of the round data
-    # Let's dump the round to a file
-    with open('market_data.json', 'w') as f:
-        json.dump(market_data, f)
     return market_data
 
 @app.route('/create_room', methods=['POST'])
